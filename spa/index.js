9f4fc203489ef010bb3a85a3638bc1cbf074d229
Jape420 <56173355+Jape420@users.noreply.github.com>
2020-05-07 21:37:47 +0200

    single page application

---
+++ spa/index.js
@@ -0,0 +1,42 @@
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".container", { duration: .3, opacity: 0, ease: Power4 });
+}
+
+function contentAnimation() {
+    var tl = gsap.timeline();
+    tl.to(".container", { duration: .4, opacity: 1, ease: Power4 });
+}
+
+barba.init({
+    sync: true,
+
+    transitions: [
+        {
+            async leave(data) {
+                const done = this.async();
+
+                pageTransition();
+                await delay(1000);
+                done();
+            },
+
+            async enter(data) {
+                contentAnimation();
+            },
+
+            async once(data) {
+                contentAnimation();
+            },
+        },
+    ],
+});
